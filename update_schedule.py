ad3f3ebf0646f354d9c13c9c0a2517e0a259c28c
Froissart Kévin <kevin.froissart69@gmail.com>
2020-11-20 22:03:17 +0000

    Update Artists

--- update_schedule.py
+++ update_schedule.py
@@ -5,7 +5,7 @@ import os
 def update_artist_list():
 	os.system('python3.8 artists.py')
 
-schedule.every().friday.at('23:02').do(update_artist_list())
+schedule.every().friday.at('23:04').do(update_artist_list())
 
 while 1:
 	schedule.run_pending()
